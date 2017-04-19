ad5a708531b0ab7081011e37534c5a0a209d2b19
joel.kinman@gmail.com <joel.kinman@gmail.com>
2017-04-19 16:47:07 +0000

    Deploy from Forestry.io

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './components/Main';
-import Resume from './components/Resume';
-require( 'fontawesome' );
-
-// Render the main component into the dom
-ReactDOM.render(<Resume />, document.getElementById('app'));
